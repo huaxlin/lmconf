83feaa939f9e0bf2e7ce82d4451c95aa891fae86
huaxlin <huaxlin33@gmail.com>
2024-07-23 22:40:50 +0800

    Draft dependencies.py

--- src/lmconf/database/__init__.py
+++ src/lmconf/database/__init__.py
@@ -1,1 +1,23 @@
 from .db_config import DatabaseConfig  # noqa: F401
+
+
+# TODO
+# class DatabaseSettingsMixinProtocol: ...
+# `dependencies.py`
+# from contextlib import contextmanager
+# def inject_db(settings_cls: Type[DatabaseSettingsMixinProtocol]):
+#     def inner(fn: Callable) -> Callable:
+#         @contextmanager
+#         def inject(kwargs):
+#             if "db" not in kwargs or kwargs["db"] is None:
+#                 with settings_cls.get_current_settings().get_db() as db:
+#                     kwargs["db"] = db
+#                     yield
+#             else:
+#                 yield
+#         @wraps(fn)
+#         def sync_wrapper(*args, **kwargs):
+#             with inject(kwargs):
+#                 return fn(*args, **kwargs)
+#         ...
+#     return inner
